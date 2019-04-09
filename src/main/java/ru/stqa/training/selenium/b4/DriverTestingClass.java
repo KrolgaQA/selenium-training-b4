64db1a7d01171235dd097143b750c2b07150453a
krasilnikova <ol.kras.andr@gmail.com>
2019-04-09 18:38:10 +0300

    Learned elements

--- src/main/java/ru/stqa/training/selenium/b4/DriverTestingClass.java
+++ src/main/java/ru/stqa/training/selenium/b4/DriverTestingClass.java
@@ -9,6 +9,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.util.concurrent.TimeUnit;
+
 import static org.openqa.selenium.support.ui.ExpectedConditions.titleIs;
 
 public class DriverTestingClass {
@@ -19,14 +21,19 @@ public class DriverTestingClass {
     @Before
     public void start(){
         driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         wait = new WebDriverWait(driver, 10);
     }
 
     @Test
     public void googlePageTest(){
         driver.get("http://www.google.com/");
+        driver.findElement(By.className("xPnqmf")).click();
+        driver.findElement(By.id("K32")).click();
+        driver.findElement(By.className("xPnqmf")).click();
+        driver.findElement(By.id("K32")).click();
         driver.findElement(By.name("q")).sendKeys("webdriver");
-        driver.findElement(By.name("btnG")).click();
+        driver.findElement(By.name("btnK")).click();
         wait.until(titleIs("webdriver - Поиск в Google"));
     }
 
