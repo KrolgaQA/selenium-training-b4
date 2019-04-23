603421fd349a42c3ec8286070131da9fb2818e17
Olga <ol.kras.andr@gmail.com>
2019-04-24 00:12:24 +0300

    Created iterator for main list items

--- src/main/java/ru/stqa/training/selenium/b4/AdminSectionChecker.java
+++ src/main/java/ru/stqa/training/selenium/b4/AdminSectionChecker.java
@@ -2,6 +2,7 @@ package ru.stqa.training.selenium.b4;
 
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -15,7 +16,7 @@ public class AdminSectionChecker {
         private WebDriver driver;
         private WebDriverWait wait;
         private List<WebElement> list;
-        private ListIterator<WebElement> li = list.listIterator();
+        private ListIterator<WebElement> li;
         private String name;
 
 
@@ -26,18 +27,33 @@ public class AdminSectionChecker {
             wait = new WebDriverWait(driver, 10);
         }
 
+        @Test
         public void adminSectionCheck() {
-            driver.get("http://localhost/litecart/admin");
-            try {
-                list = driver.findElements(By.className(""));
-            }
-            catch (Exception e){
-                System.out.println("Can't find element");
-            }
-            while (li.hasNext()){
-                li.next().click();
-                name = driver.findElement(By.tagName("h1")).getText();
-                System.out.println(name);
+            driver.get("http://localhost/litecart/public_html/admin/");
+            driver.findElement(By.name("username")).clear();
+            driver.findElement(By.name("username")).sendKeys("admin");
+            driver.findElement(By.name("password")).clear();
+            driver.findElement(By.name("password")).sendKeys("admin");
+            driver.findElement(By.tagName("button")).click();
+//            list = driver.findElements(By.cssSelector("ul#box-apps-menu li"));
+//            li = list.listIterator();
+//
+//            while (li.hasNext()){
+//                li.next().click();
+//                name = driver.findElement(By.tagName("h1")).getText();
+//                System.out.println(name);
+//                driver.get("http://localhost/litecart/public_html/admin/");
+//                list = driver.findElements(By.cssSelector("ul#box-apps-menu li"));
+//            }
+//            System.out.println("the end");
+            int size = 1;
+
+            for (int i = 0 ; i < size ; ++i) {
+                list = driver.findElements(By.cssSelector("ul#box-apps-menu li")); //locate all the links
+                size = list.size(); //change the loop itreations number to the right one
+                list.get(i).click(); //click on the link
+                System.out.println(driver.findElement(By.tagName("h1")).getText()); //print the data
+                driver.navigate().back(); //go to previous page
             }
         }
 
--- src/main/java/ru/stqa/training/selenium/b4/LitecartLoginTest.java
+++ src/main/java/ru/stqa/training/selenium/b4/LitecartLoginTest.java
@@ -6,37 +6,41 @@ import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.concurrent.TimeUnit;
 
 public class LitecartLoginTest {
-    private WebDriver driver;
+//    WebDriver chromeDriver = new ChromeDriver();
+//    WebDriver ieDriver = new InternetExplorerDriver();
+    WebDriver firefoxDriver = new FirefoxDriver();
     private WebDriverWait wait;
 
 
     @Before
     public void start() {
-        driver = new ChromeDriver();
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        wait = new WebDriverWait(driver, 10);
+
+        firefoxDriver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        wait = new WebDriverWait(firefoxDriver, 10);
     }
 
     @Test
     public void letsLogin() {
-        driver.get("http://localhost/litecart/public_html/");
-        driver.findElement(By.cssSelector("#default-menu > ul.nav.navbar-nav.navbar-right > li")).click();
-        driver.findElement(By.name("email")).clear();
-        driver.findElement(By.name("email")).sendKeys("admin");
-        driver.findElement(By.name("password")).clear();
-        driver.findElement(By.name("password")).sendKeys("admin");
-        driver.findElement(By.name("login")).click();
+        firefoxDriver.get("http://localhost/litecart/public_html/");
+        firefoxDriver.findElement(By.cssSelector("#default-menu > ul.nav.navbar-nav.navbar-right > li")).click();
+        firefoxDriver.findElement(By.name("email")).clear();
+        firefoxDriver.findElement(By.name("email")).sendKeys("admin");
+        firefoxDriver.findElement(By.name("password")).clear();
+        firefoxDriver.findElement(By.name("password")).sendKeys("admin");
+        firefoxDriver.findElement(By.name("login")).click();
     }
 
     @After
     public void stop() {
-        driver.quit();
-        driver = null;
+        firefoxDriver.quit();
+        firefoxDriver = null;
     }
 
 }
