471d782f47d1614339901808c1750dbeb549dddc
Olga <ol.kras.andr@gmail.com>
2019-04-29 13:10:01 +0300

    Completed AdminSectionChecker

--- src/main/java/ru/stqa/training/selenium/b4/AdminSectionChecker.java
+++ src/main/java/ru/stqa/training/selenium/b4/AdminSectionChecker.java
@@ -1,6 +1,7 @@
 package ru.stqa.training.selenium.b4;
 
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
@@ -8,70 +9,68 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.ArrayList;
 import java.util.List;
-import java.util.ListIterator;
 import java.util.concurrent.TimeUnit;
 
 public class AdminSectionChecker {
-        private WebDriver driver;
-        private WebDriverWait wait;
-        private List<WebElement> list;
-        private List<WebElement> listSmall;
-//        private ListIterator<WebElement> li;
+    private WebDriver driver;
+    private WebDriverWait wait;
+    private List<WebElement> list = new ArrayList<>();
+    private List<WebElement> subList = new ArrayList<>();
+    private WebElement el;
+    public ArrayList<String> locators = new ArrayList<>();
+    public ArrayList<String> links = new ArrayList<>();
+    public String locator;
+    public String link;
 
+    @Before
+    public void start() {
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(1, TimeUnit.SECONDS);
+        wait = new WebDriverWait(driver, 1);
+    }
 
-        @Before
-        public void start() {
-            driver = new ChromeDriver();
-            driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
-            wait = new WebDriverWait(driver, 10);
+    @Test
+    public void adminSectionCheck() {
+        driver.get("http://localhost/litecart/public_html/admin/");
+        driver.findElement(By.name("username")).clear();
+        driver.findElement(By.name("username")).sendKeys("admin");
+        driver.findElement(By.name("password")).clear();
+        driver.findElement(By.name("password")).sendKeys("admin");
+        driver.findElement(By.tagName("button")).click();
+        list = driver.findElements(By.cssSelector("ul#box-apps-menu li"));
+        if (list.size() > 0) {
+            for (int i = 0; i < list.size(); i++) {
+                locator = list.get(i).getAttribute("id");
+                locators.add(locator);
+            }
         }
 
-        @Test
-        public void adminSectionCheck() {
-            driver.get("http://localhost/litecart/public_html/admin/");
-            driver.findElement(By.name("username")).clear();
-            driver.findElement(By.name("username")).sendKeys("admin");
-            driver.findElement(By.name("password")).clear();
-            driver.findElement(By.name("password")).sendKeys("admin");
-            driver.findElement(By.tagName("button")).click();
-//            list = driver.findElements(By.cssSelector("ul#box-apps-menu li"));
-//            li = list.listIterator();
-//
-//            while (li.hasNext()){
-//                li.next().click();
-//                name = driver.findElement(By.tagName("h1")).getText();
-//                System.out.println(name);
-//                driver.get("http://localhost/litecart/public_html/admin/");
-//                list = driver.findElements(By.cssSelector("ul#box-apps-menu li"));
-//            }
-            int size = 1;
-            int sizeSmall = 1;
-
-            for (int i = 0 ; i < size ; ++i) {
-                list = driver.findElements(By.cssSelector("ul#box-apps-menu li"));
-                size = list.size();
-                String s = list.get(i).getAttribute("id");
-                list.get(i).click();
-                System.out.println(driver.findElement(By.tagName("h1")).getText());
-                listSmall = driver.findElements(By.cssSelector("li#" + s + "ul.docs li"));
-                sizeSmall = listSmall.size();
-                if(sizeSmall>0){
-                for (int j = 0; j < sizeSmall; ++j) {
-                    if (!listSmall.get(j).isSelected()) {
-                        listSmall.get(j).click();
-                        System.out.println(driver.findElement(By.tagName("h1")).getText());
-                        driver.navigate().back();
-                    }
-                }
+        for (int i = 0; i < locators.size(); i++) {
+            locator = locators.get(i);
+            el = driver.findElement(By.id(locator));
+            el.click();
+            Assert.assertTrue(driver.findElement(By.tagName("h1")).isDisplayed());
+//            System.out.println(driver.findElement(By.tagName("h1")).getText());
+            subList = driver.findElements(By.cssSelector("#" + locator + " li"));
+            if (subList.size() > 0) {
+                links.clear();
+                for (int j = 0; j < subList.size(); j++) {
+                    link = subList.get(j).findElement(By.tagName("a")).getAttribute("href");
+                    links.add(link);
                 }
             }
-            driver.navigate().back();
-
+            for (int k = 0; k < links.size(); k++) {
+                driver.get(links.get(k));
+                Assert.assertTrue(driver.findElement(By.tagName("h1")).isDisplayed());
+            }
         }
+    }
 
     @After
-    public void stop(){
+    public void stop() {
         driver.quit();
         driver = null;
     }
