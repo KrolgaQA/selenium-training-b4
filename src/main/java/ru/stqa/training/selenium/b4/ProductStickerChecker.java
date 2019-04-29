bc7362ead8d4f2db296f30eefddb6e8978481753
Olga <ol.kras.andr@gmail.com>
2019-04-29 16:07:40 +0300

    Completed ProductStickerChecker

--- src/main/java/ru/stqa/training/selenium/b4/ProductStickerChecker.java
+++ src/main/java/ru/stqa/training/selenium/b4/ProductStickerChecker.java
@@ -1,6 +1,7 @@
 package ru.stqa.training.selenium.b4;
 
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
@@ -9,14 +10,14 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 public class ProductStickerChecker {
     private WebDriver driver;
     private WebDriverWait wait;
-    private WebElement el;
-    public List<WebElement> list;
+    public List<WebElement> list = new ArrayList<>();
     @Before
     public void start() {
         driver = new ChromeDriver();
@@ -26,18 +27,10 @@ public class ProductStickerChecker {
     @Test
     public void productStickerCheck(){
         driver.get("http://localhost/litecart/public_html/");
-        int size = 1;
-        int size1 = 1;
-        for (int i = 0; i <size ; i++) {
-            el = driver.findElement(By.cssSelector("#content > ul"));
-            list = el.findElements(By.cssSelector("li"));
-            size = list.size();
-            list.get(i).click();
-//            System.out.println(list.get(i).getText());
-            for (int j = 0; j <size1 ; j++) {
-                list = driver.findElements(By.cssSelector("#box-campaign-products > div > div"));
-                el = list.get(j);
-            }
+        list = driver.findElements(By.xpath("//div[contains(@class,'col-xs-6')]"));
+        System.out.println(list.size());
+        for (int i = 0; i < list.size(); i++) {
+            Assert.assertEquals(list.get(i).findElements(By.xpath(".//div[contains(@class, 'sticker')]")).size(), 1);
         }
     }
     @After
